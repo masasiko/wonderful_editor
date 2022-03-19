f762c86c01cf4919facf120ca802f317ca982bd1
高橋雅彦 <m.taka1238@gmail.com>
2022-03-19 11:23:05 +0900

    controllerのRspecの作成 routesを階層化

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for "User", at: "auth"
-  # controllers:
-  # {
-  #   registrations: 'auth/registrations'
-  # }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace "api" do
+    namespace "v1" do
+      mount_devise_token_auth_for "User", at: "auth"
+      resources :article
+    end
+  end
 end
+
+# controllers:
+# {
+#   registrations: 'auth/registrations'
+# }
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
