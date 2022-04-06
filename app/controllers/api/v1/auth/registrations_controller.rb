2745626860fd2eacb74c3684aa43941d9c62805e
高橋雅彦 <m.taka1238@gmail.com>
2022-04-06 23:29:49 +0900

    account_update_paramsの追記

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -5,4 +5,8 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
     def sign_up_params
       params.permit(:name, :email, :password, :password_confirmation)
     end
+
+    def account_update_params
+      params.permit(:name, :email)
+    end
 end
