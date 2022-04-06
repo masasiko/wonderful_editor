c04023f65d8dc18ee61d3dd5af984cd0ac86fd45
高橋雅彦 <m.taka1238@gmail.com>
2022-04-06 23:26:59 +0900

    rubocopにて修正

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,8 +1,8 @@
-class Api::V1::Auth::RegistrationsController <  DeviseTokenAuth::RegistrationsController
-  binding.pry
+class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
 
   private
-  def sign_up_params
-  params.permit(:name,:email, :password)
-  end
+
+    def sign_up_params
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
 end
