414fe07a3a739f9b21190946cec5fb8920d39c78
高橋雅彦 <m.taka1238@gmail.com>
2022-04-24 09:32:34 +0900

    articleのshowを公開記事のみ表示するように実装

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,23 +1,20 @@
 class Api::V1::ArticlesController < Api::V1::BaseApiController
-   before_action :authenticate_api_v1_user!, only: [:create, :update, :destroy] # ログインユーザーでなければ実行されない
+  before_action :authenticate_api_v1_user!, only: [:create, :update, :destroy] # ログインユーザーでなければ実行されない
 
   def index
-    binding.pry
     Article.all
-    binding.pry
     article = Article.published.order("updated_at DESC")
-    binding.pry
     render json: article, each_serializer: Api::V1::ArticlePreviewSerializer
   end
 
   def show
-    article = Article.find(params[:id])
+    article = Article.published.find(params[:id])
     render json: article, serializer: Api::V1::ArticleSerializer
   end
 
   def create
     article = Article.new(article_params)
-    article.user_id = current_api_v1_user.id# ログインユーザーのuser_idになる
+    article.user_id = current_api_v1_user.id # ログインユーザーのuser_idになる
     article.save!
     render json: article, serializer: Api::V1::ArticleSerializer
   end
