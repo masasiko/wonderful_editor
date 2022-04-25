f4ad1f2676d7c40af2f8c1f228edb818d98cd75d
高橋雅彦 <m.taka1238@gmail.com>
2022-04-25 22:54:13 +0900

    current/articles_controllerを作成 index、showメソッドを作成 ログインユーザーのみの公開記事の表示ができるように実装

--- app/controllers/api/v1/current/articles_controller.rb
+++ app/controllers/api/v1/current/articles_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::Current::ArticlesController < ApplicationController
+  before_action :authenticate_api_v1_user!
+
+  def index
+    Article.all
+    article = current_api_v1_user.articles.published.order("updated_at DESC")
+    binding.pry
+    render json: article, each_serializer: Api::V1::ArticlePreviewSerializer
+  end
+
+  def show
+    article = current_api_v1_user.articles.published.find(params[:id])
+    render json: article, serializer: Api::V1::ArticleSerializer
+  end
+
+
+
+
+
+
+
+
+
 end
